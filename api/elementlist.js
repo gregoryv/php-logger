07a836366fc1c7dcd5d409bbeab071699c5fe699
Gregory Vinčić <g@7de.se>
2015-05-10 08:47:49 +0200

    First generated api

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","gregoryv\\logger\\CachedWriter"],["c","gregoryv\\logger\\Logger"],["c","gregoryv\\logger\\PriorityWriterInterface"],["c","gregoryv\\logger\\SyslogWriter"]];
