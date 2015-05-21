90846cb3d31b0c90a31647d2adec257af1021516
Gregory Vinčić <g@7de.se>
2015-05-21 16:53:57 +0200

    API updates

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","gregoryv\\logger\\CachedWriter"],["c","gregoryv\\logger\\FileWriter"],["c","gregoryv\\logger\\Logger"],["c","gregoryv\\logger\\PriorityWriterInterface"],["c","gregoryv\\logger\\SyslogWriter"]];
+ApiGen.elements = [["c","gregoryv\\logger\\CachedWriter"],["c","gregoryv\\logger\\FileWriter"],["c","gregoryv\\logger\\Logger"],["c","gregoryv\\logger\\SeverityWriterInterface"],["c","gregoryv\\logger\\SyslogWriter"]];
