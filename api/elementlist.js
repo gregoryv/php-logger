39ffd0f1f31fdc44896dea94acc803059756ad40
Gregory Vinčić <g@7de.se>
2015-05-29 09:50:58 +0200

    API additions for ConsoleWriter

--- api/elementlist.js
+++ api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","gregoryv\\logger\\CachedWriter"],["c","gregoryv\\logger\\FileWriter"],["c","gregoryv\\logger\\Logger"],["c","gregoryv\\logger\\SeverityWriterInterface"],["c","gregoryv\\logger\\State"],["c","gregoryv\\logger\\SyslogWriter"]];
+ApiGen.elements = [["c","gregoryv\\logger\\CachedWriter"],["c","gregoryv\\logger\\ConsoleWriter"],["c","gregoryv\\logger\\FileWriter"],["c","gregoryv\\logger\\Logger"],["c","gregoryv\\logger\\SeverityWriterInterface"],["c","gregoryv\\logger\\State"],["c","gregoryv\\logger\\SyslogWriter"]];
